8df5148d98d0842dc38a7b09b8e1c1dc8f01eb0a
adee-dev <75178138+adee-dev@users.noreply.github.com>
2022-04-06 23:34:21 +0530

    practice questions update

---
+++ alive/dmo.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+ string arr[3];
+ arr[0]="hello";
+ arr[1]="hello";
+ arr[2]="hello";
+    //cout<<"\nString array:\n";
+for (int x = 0; x< 3; x++) {
+    cout << arr[x] << "\n"; 
+	} 
+	return 0;
+}
