1234ebadcae25e8ac2e1dddbefa552611a300687
adee-dev <75178138+adee-dev@users.noreply.github.com>
2022-05-04 15:26:33 +0530

    practice questions

---
+++ alive/richie_rich.cpp
@@ -0,0 +1,24 @@
+/*
+Aditya Dayal's template for codechef!<3!
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define fr(n) for(int i=0; i<n; i++)
+#define fri(a,n) for(int i=a; i<n; i++)
+#define int long long
+#define OwO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void solve(){
+	int a,b,x;cin>>a>>b>>x;
+	cout<<(b-a)/x<<endl;
+}
+	
+int32_t main(){	
+	OwO;
+	int UwU;cin>>UwU;
+	while(UwU--){
+		solve();
+	}
+	return 0;
+}
