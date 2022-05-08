feee33dd1b3da1d0b368ebcaa52c03e10b9114b0
adee-dev <75178138+adee-dev@users.noreply.github.com>
2022-05-08 12:37:34 +0530

    practice questions

---
+++ alive/hoops.cpp
@@ -0,0 +1,24 @@
+/*
+Aditya Dayal's template for codechef!<3!
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define fr(n) for(int i=0; i<n; i++)
+#define fri(a,n) for(int i=a; i<n; i++)
+#define int long long
+#define OwO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void solve(){
+	int n;cin>>n;
+	cout<<(n/2)+(n%2!=0)<<endl;
+}
+	
+int32_t main(){	
+	OwO;
+	int UwU;cin>>UwU;
+	while(UwU--){
+		solve();
+	}
+	return 0;
+}
