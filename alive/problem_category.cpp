99134ef226f3dfa6ba1ee1d629b153037c56a7fa
adee-dev <75178138+adee-dev@users.noreply.github.com>
2022-05-10 23:55:04 +0530

    practice problems

--- alive/problem_category.cpp
+++ alive/problem_category.cpp
@@ -10,11 +10,10 @@ using namespace std;
 #define OwO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 void solve(){
-	int a,b,a1,b1,a2,b2;
-	cin>>a>>b>>a1>>b1>>a2>>b2;
-	if(((a==a1) || (a==b1)) && ((b==a1) || (b==b1))) cout<<1<<endl;
-	else if(((a==a2) || (a==b2)) && ((b==a2) || (b==b2))) cout<<2<<endl;
-	else cout<<0<<endl;
+	int x;cin>>x;
+	if(x>=1 && x<100) cout<<"Easy"<<endl;
+	else if(x>=100 && x<200) cout<<"Medium"<<endl;
+	else cout<<"Hard"<<endl;
 }
 	
 int32_t main(){	
