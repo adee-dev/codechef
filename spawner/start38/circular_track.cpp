8f6059bf776df80c0ea6b2543c7c7cd7f9f96e5c
adee-dev <75178138+adee-dev@users.noreply.github.com>
2022-05-12 00:39:30 +0530

    starter 38

---
+++ spawner/start38/circular_track.cpp
@@ -0,0 +1,33 @@
+/**
+ * Author: Aditya Dayal
+ * Platform: Codechef
+ * Enjoy!!! <3
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define pb push_back
+#define pf push_front
+#define all(var) var.begin(), var.end()
+
+#define endl "\n"
+#define fr(n) for(int i=0; i<n; i++)
+#define fri(a,n) for(int i=a; i<n; i++)
+// #define int long long
+#define OwO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+//-----------------------------:)----------------------------------
+
+void solve(){
+	int a,b,m;cin>>a>>b>>m;
+	cout<<min(((min(a,b)+m)-max(a,b)),(max(a,b)-min(a,b)))<<endl;
+}
+	
+int32_t main(){	
+	OwO;
+	int UwU;cin>>UwU;
+	while(UwU--) solve();
+	return 0;
+}
